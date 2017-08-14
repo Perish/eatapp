ebfee0e03751694a1fbfddf7f24ec6d8f1bc0378
Rubist <rubist@RubistdeMacBook-Pro.local>
2017-08-14 18:26:58 +0800

    add-cell-and-data-manager

---
+++ LetsEat/ExploreCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExploreCell.swift
+//  LetsEat
+//
+//  Created by Rubist on 2017/8/14.
+//  Copyright © 2017年 Rubist. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCell: UICollectionViewCell {
+    @IBOutlet var lblName:UILabel!
+    @IBOutlet var imageExplore:UIImageView!
+}
--- LetsEat/ExploreViewController.swift
+++ LetsEat/ExploreViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class ExploreViewController: UIViewController, UICollectionViewDataSource {
 
     @IBOutlet var collectionView:UICollectionView!
+    let manager = ExploreDataManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager.fetch()
         // Do any additional setup after loading the view.
     }
     
@@ -23,11 +25,19 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource {
     }
 //    设置每个部分中有多少不同的条目需要展示
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return manager.numberOfItems()
     }
 //   条目设置
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath) as! ExploreCell
+        
+        let item = manager.explore(at: indexPath)
+        if let name = item.name {
+            cell.lblName.text = name
+        }
+        if let image = item.image {
+            cell.imageExplore.image = UIImage(named: image)
+        }
         
         return cell
     }
